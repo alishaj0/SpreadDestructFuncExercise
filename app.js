71df5f7418994bd1a7fcb8c05ba35fbd464fde36
Alisha Bolin <alishabolin@gmail.com>
2021-03-18 13:41:34 -0400

    Commit

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ function seeDinos(a, ...b){
     console.log(b);
 }
 seeDinos(...jPDinos);
-                // to ...spread the remainder of array among 'b' parameter
+                // to ...spread argument on the remainder of array among 'b' parameter
+                // spreading out contents of array with ...spread
+                // using (...) rest 2nd argument and on will be grouped together inside parameter
 
                         // Velociraptors
                         // [ 'Tryannosaurus Rex', 'Dilophasaurus' ]
@@ -38,21 +40,24 @@ seeDinos(...jPDinos);
 //2a)
 const jPCharacters = {
     alanGrant: "Sam Neill",
-    ellieSattlet: "Laura Dern",
+    ellieSattler: "Laura Dern",
     ianMalcolm: "Jeff Goldblum"
 };
 
 //2b)
     // without destructuring
+        // using parameter (name) with key property (name.ellieStattler) in console.log to display key value when run
 // function seeCharacters(name){
 //     console.log(name.ellieSattlet);
 // }
 // seeCharacters(jPCharacters);
                         // Laura Dern
  
-    // with destructuring
-function seeCharacters({ellieSattlet}){
-    console.log(ellieSattlet);
+                    
+    // with destructuring 
+        // using the {key property} inside parameter with {} to display key value when run)
+function seeCharacters({ellieSattler}){
+    console.log(ellieSattler);
 }
 seeCharacters(jPCharacters);
                         // Laura Dern
@@ -70,6 +75,7 @@ const jurassicParkMovies = [
 ];
 
 function jPMovies({one, two, three}, {four, five}){
+        // ...spreading content of jurassicParkMovies array by destructuring the two objects within the array
     console.log(one);
     console.log(two);
     console.log(three);
@@ -82,3 +88,16 @@ jPMovies(...jurassicParkMovies);
                         // Jurassic Park III
                         // Jurassic World
                         // Jurassic World: Fallen Kingdom
+
+        //OR
+
+        // function jPMovies([{one, two, three}, {four, five}]){
+                // Using [{}] in parameter for array and to destructure the entire array??. 
+        //     console.log(one);
+        //     console.log(two);
+        //     console.log(three);
+        //     console.log(four);
+        //     console.log(five);
+        // }
+        // jPMovies(jurassicParkMovies);
+
